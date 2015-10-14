a7d668d3257201b255afdc4676d99c9041c09676
Magnus <jumalalammas@gmail.com>
2015-10-14 18:26:39 +0300

    Rotating pyramid and cube

--- main/Main.java
+++ main/Main.java
@@ -1,39 +1,64 @@
 package main;
 
+import java.awt.Dimension;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
-import com.jogamp.opengl.GLCapabilities;
-import com.jogamp.opengl.GLProfile;
-import com.jogamp.opengl.awt.GLCanvas;
 import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+import com.jogamp.opengl.awt.GLCanvas;
+import com.jogamp.opengl.util.FPSAnimator;
 
 public class Main {
 
-	public static int canvasHeight = 800;
-	public static int canvasWidth = 600;
+	// Define constants for the top-level container
+	private static String TITLE = "Akna pealkiri";								
+	private static final int CANVAS_WIDTH = 600; // width of the drawable
+	private static final int CANVAS_HEIGHT = 600; // height of the drawable
+	private static final int FPS = 60; // animator's target frames per second
 
+	/** The entry main() method to setup the top-level container and animator */
 	public static void main(String[] args) {
-		// setup OpenGL Version 2
-		GLProfile profile = GLProfile.get(GLProfile.GL2);
-		GLCapabilities capabilities = new GLCapabilities(profile);
+		// Run the GUI codes in the event-dispatching thread for thread safety
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				// Create the OpenGL rendering canvas
+				GLCanvas canvas = new Rotate();
+				canvas.setPreferredSize(new Dimension(CANVAS_WIDTH, CANVAS_HEIGHT));
 
-		// The canvas is the widget that's drawn in the JFrame
-		GLCanvas glcanvas = new GLCanvas(capabilities);
-		glcanvas.addGLEventListener(new Renderer());
-		glcanvas.setSize(canvasHeight, canvasWidth);
+				// Create a animator that drives canvas' display() at the
+				// specified FPS.
+				final FPSAnimator animator = new FPSAnimator(canvas, FPS, true);
 
-		JFrame frame = new JFrame("Hello World");
-		frame.getContentPane().add(glcanvas);
+				// Create the top-level container
+				final JFrame frame = new JFrame(); // Swing's JFrame or AWT's
+													// Frame
+				frame.getContentPane().add(canvas);
+				frame.addWindowListener(new WindowAdapter() {
+					@Override
+					public void windowClosing(WindowEvent e) {
+						// Use a dedicate thread to run the stop() to ensure
+						// that the
+						// animator stops before program exits.
+						new Thread() {
+							@Override
+							public void run() {
+								if (animator.isStarted()) {
+									animator.stop();
+								}
 
-		// shutdown the program on windows close event
-		frame.addWindowListener(new WindowAdapter() {
-			public void windowClosing(WindowEvent ev) {
-				System.exit(0);
+								System.exit(0);
+							}
+						}.start();
+					}
+				});
+				frame.setTitle(TITLE);
+				frame.pack();
+				frame.setVisible(true);
+				animator.start(); // start the animation loop
 			}
 		});
-
-		frame.setSize(frame.getContentPane().getPreferredSize());
-		frame.setVisible(true);
 	}
 }
