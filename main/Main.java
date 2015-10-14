ebc66b376ef86fc9faa5d4b6939011b498bed18b
Magnus <jumalalammas@gmail.com>
2015-10-14 14:55:38 +0300

    Triangle and square demo

--- main/Main.java
+++ main/Main.java
@@ -1,14 +1,35 @@
 package main;
 
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import com.jogamp.opengl.GLCapabilities;
+import com.jogamp.opengl.GLProfile;
+import com.jogamp.opengl.awt.GLCanvas;
+import javax.swing.JFrame;
+
 public class Main {
-
-	public Main() {
-		// TODO Auto-generated constructor stub
-	}
-
 	public static void main(String[] args) {
-		Game game = new Game();
-		game.play();
-	}
+		// setup OpenGL Version 2
+		GLProfile profile = GLProfile.get(GLProfile.GL2);
+		GLCapabilities capabilities = new GLCapabilities(profile);
 
+		// The canvas is the widget that's drawn in the JFrame
+		GLCanvas glcanvas = new GLCanvas(capabilities);
+		glcanvas.addGLEventListener(new Renderer());
+		glcanvas.setSize(300, 300);
+
+		JFrame frame = new JFrame("Hello World");
+		frame.getContentPane().add(glcanvas);
+
+		// shutdown the program on windows close event
+		frame.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent ev) {
+				System.exit(0);
+			}
+		});
+
+		frame.setSize(frame.getContentPane().getPreferredSize());
+		frame.setVisible(true);
+	}
 }
