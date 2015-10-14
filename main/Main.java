1c71a87ce85b5f6b29e0156350b6feceea9355e2
Magnus <jumalalammas@gmail.com>
2015-10-14 13:52:22 +0300

    Created empty opengl window, using default GLProfile

---
+++ main/Main.java
@@ -0,0 +1,14 @@
+package main;
+
+public class Main {
+
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		 Game game = new Game();
+		 game.play();
+	}
+
+}
