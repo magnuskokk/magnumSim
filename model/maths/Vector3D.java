02a276044b4283e00af15c4b84bb17c174fccb62
Magnus <jumalalammas@gmail.com>
2015-10-18 03:18:30 +0300

    Updated some vector logic

--- model/maths/Vector3D.java
+++ model/maths/Vector3D.java
@@ -2,20 +2,46 @@ package model.maths;
 
 public class Vector3D {
 
+	public double x, y, z;
 	public Point3D begin, end;
 	
 	// Define by begin and end points
 	public Vector3D(Point3D begin, Point3D end) {
 		this.begin = begin;
 		this.end = end;
+		
+		this.x = end.x - begin.x;
+		this.y = end.y - begin.y;
+		this.z = end.z - begin.z;
+	}
+
+	// Define by vector coordinates
+	public Vector3D(double x, double y, double z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+		
+		begin = null;
+		end = null;
 	}
 
 	public void multiply(double mul) {
-		begin.multiply(mul);
-		end.multiply(mul);
+		x *= mul;
+		y *= mul;
+		z *= mul;
+		
+		if (begin != null && end != null) {
+			begin.multiply(mul);
+			end.multiply(mul);
+		}
+	}
+	
+	public void add(Vector3D vector) {
+		
+		
 	}
 	
 	public double length() {
-		return Math.sqrt(Math.pow(end.x - begin.x, 2) + Math.pow(end.y - begin.y, 2) + Math.pow(end.z - begin.z, 2));
+		return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2) + Math.pow(z, 2));
 	}
 }
