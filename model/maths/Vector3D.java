3307da207bac0a4bf65fe0671e775909731da5d7
Magnus <jumalalammas@gmail.com>
2015-10-19 10:59:03 +0300

    Separate vectors with coordinates and points

--- model/maths/Vector3D.java
+++ model/maths/Vector3D.java
@@ -1,40 +1,20 @@
 package model.maths;
 
-//TODO maybe extend this class to create the camera vector separately
 public class Vector3D {
 
 	public double x, y, z;
-	public Point3D begin, end;
-	
-	// Define by begin and end points
-	public Vector3D(Point3D begin, Point3D end) {
-		this.begin = begin;
-		this.end = end;
-		
-		this.x = end.x - begin.x;
-		this.y = end.y - begin.y;
-		this.z = end.z - begin.z;
-	}
 
 	// Define by vector coordinates
 	public Vector3D(double x, double y, double z) {
 		this.x = x;
 		this.y = y;
 		this.z = z;
-		
-		begin = null;
-		end = null;
 	}
 
-	public Vector3D multiply(double mul) {
-		x *= mul;
-		y *= mul;
-		z *= mul;
-		
-		if (begin != null && end != null) {
-			begin.multiply(mul);
-			end.multiply(mul);
-		}
+	public Vector3D multiply(double scalar) {
+		x *= scalar;
+		y *= scalar;
+		z *= scalar;
 		
 		return this;
 	}
---
+++ model/maths/Vector3DWithPoints.java
@@ -0,0 +1,18 @@
+package model.maths;
+
+public class Vector3DWithPoints {
+
+	public Point3D begin, end;
+	
+	// Define by begin and end points
+	public Vector3DWithPoints(Point3D begin, Point3D end) {
+		this.begin = begin;
+		this.end = end;
+	}
+	
+	public Vector3DWithPoints multiply() {
+		
+	}
+
+
+}
