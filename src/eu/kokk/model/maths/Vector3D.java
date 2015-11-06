3a28b1668d720c93c44218c1273a754aa2fe3a0b
Magnus <jumalalammas@gmail.com>
2015-11-06 05:05:53 +0200

    optimise a bit

--- src/eu/kokk/model/maths/Vector3D.java
+++ src/eu/kokk/model/maths/Vector3D.java
@@ -162,7 +162,7 @@ public class Vector3D {
      * @return Length
      */
     public double length() {
-        return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2) + Math.pow(z, 2));
+        return Math.sqrt(this.x * this.x + this.y * this.y + this.z * this.z);
     }
 
     /**
